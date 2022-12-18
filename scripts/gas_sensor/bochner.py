a3be9fb226c39572bf463eda25b10f584b3b78cf
mathtenote01 <32406925+mathtenote01@users.noreply.github.com>
2022-12-18 22:34:30 +0900

    Update bochner.py

--- scripts/gas_sensor/bochner.py
+++ scripts/gas_sensor/bochner.py
@@ -259,7 +259,7 @@ def main(
             # )
             # evaluation_error.backward()
             # meta_train_error += evaluation_error.item()
-        with futures.ThreadPoolExecutor() as executor:
+        with futures.ThreadPoolExecutor(max_workers=meta_batch_size) as executor:
             for train_batch in train_batches:
                 # タスクを追加する。
                 executor.submit(_fast_adapt_boch_train, train_batch, model, loss, D, device)
@@ -274,7 +274,7 @@ def main(
             #         device=device,
             #     )
             #     meta_valid_error += evaluation_error.item()
-            with futures.ThreadPoolExecutor() as executor:
+            with futures.ThreadPoolExecutor(max_workers=meta_val_batch_size) as executor:
                 for val_batch in val_batches:
                     # タスクを追加する。
                     executor.submit(_fast_adapt_boch_valid, val_batch, model, loss, D, device)
